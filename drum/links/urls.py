3c0d3798c468cfc390dc40e4c79b71057192a964
Emilie <zawadzki@ircam.fr>
2017-05-17 15:45:18 +0200

    [Auth] : add login required

--- drum/links/urls.py
+++ drum/links/urls.py
@@ -4,37 +4,38 @@ from django.conf.urls import url
 from django.contrib.auth.decorators import login_required
 
 from drum.links.views import LinkList, LinkCreate, LinkDetail, CommentList, TagList
+from django.contrib.auth.decorators import login_required
 
 
 urlpatterns = [
     url("^$",
-        LinkList.as_view(),
+        login_required(LinkList.as_view()),
         name="home"),
     url("^newest/$",
-        LinkList.as_view(), {"by_score": False},
+        login_required(LinkList.as_view()), {"by_score": False},
         name="link_list_latest"),
     url("^comments/$",
-        CommentList.as_view(), {"by_score": False},
+        login_required(CommentList.as_view()), {"by_score": False},
         name="comment_list_latest"),
     url("^best/$",
-        CommentList.as_view(),
+        login_required(CommentList.as_view()),
         name="comment_list_best"),
     url("^link/create/$",
         login_required(LinkCreate.as_view()),
         name="link_create"),
     url("^link/(?P<slug>.*)/$",
-        LinkDetail.as_view(),
+        login_required(LinkDetail.as_view()),
         name="link_detail"),
     url("^users/(?P<username>.*)/links/$",
-        LinkList.as_view(), {"by_score": False},
+        login_required(LinkList.as_view()), {"by_score": False},
         name="link_list_user"),
     url("^users/(?P<username>.*)/comments/$",
-        CommentList.as_view(), {"by_score": False},
+        login_required(CommentList.as_view()), {"by_score": False},
         name="comment_list_user"),
     url("^tags/$",
-        TagList.as_view(),
+        login_required(TagList.as_view()),
         name="tag_list"),
     url("^tags/(?P<tag>.*)/$",
-        LinkList.as_view(),
+        login_required(LinkList.as_view()),
         name="link_list_tag"),
 ]
